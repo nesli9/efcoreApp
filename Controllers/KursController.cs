7e022f20bed432f4bf6a0ec87d9fc7df8a7169cf
Neslihan Doğan <74837454+nesli9@users.noreply.github.com>
2024-04-16 13:52:18 +0300

    Kurs Öğrencilerinin Listelenmesi

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -35,7 +35,11 @@ namespace efcoreApp.Controllers{
                 return NotFound();
             }
 
-            var kurs = await _context.Kurslar.FindAsync(id);
+            var kurs = await _context
+                                    .Kurslar
+                                    .Include(k => k.KursKayitlari)
+                                    .ThenInclude(k => k.Ogrenci)
+                                    .FirstOrDefaultAsync(k => k.KursId == id);
             
             if(kurs == null){
                 return NotFound();
